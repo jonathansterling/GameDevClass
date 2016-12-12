ce4a3f6d5551486de1e2c84da7ab1904c6460187
AaronV-T <avtholl@hotmail.com>
2016-12-12 12:22:17 -0700

    Test Level

--- Assets/Scripts/AaronTestLevelController.cs
+++ Assets/Scripts/AaronTestLevelController.cs
@@ -15,8 +15,15 @@ public class AaronTestLevelController : BaseLevelController {
         UnitSpawner.SpawnUnit(UnitReferences.EnemyCruiserLight1, new Vector3(100, 50, 100));
         UnitSpawner.SpawnUnit(UnitReferences.AlliedCruiserHeavy1, new Vector3(0, 50, -200), Quaternion.Euler(0,0,0));
 
-        UnitSpawner.SpawnUnitsInArea(UnitReferences.EnemyFighter1, 100, enemySpawns[0]);
-        UnitSpawner.SpawnUnitsInArea(UnitReferences.AlliedFighter1, 20, allySpawns[0]);
+        UnitSpawner.SpawnUnitsInArea(UnitReferences.EnemyFighter1, 75, enemySpawns[0]);
+        UnitSpawner.SpawnUnitsInArea(UnitReferences.AlliedFighter1, 15, allySpawns[0]);
     }
-	
+
+    protected override void UnitDeath()
+    {
+        if (UnitTracker.GetActiveEnemyCount() == 0)
+            UnitSpawner.SpawnUnit(UnitReferences.EnemyBattleship, new Vector3(0, 100, 200));
+        
+    }
+
 }
